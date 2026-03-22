d9bde4565240473af0b5e04d4917b301e4c79cf4
Ruan Cancio <slidruan@gmail.com>
2026-03-22 13:20:22 -0300

    Backend version 1 finally!

--- backend/src/routes/file.routes.ts
+++ backend/src/routes/file.routes.ts
@@ -4,10 +4,9 @@ import { authMiddleware } from "../middlewares/auth.middleware";
 import { prisma } from "../database/prisma"
 import fs from "fs"
 import path from "path";
-import { error } from "console";
-
 const router = Router()
 
+//POST
 router.post(
     "/upload",
     authMiddleware,
@@ -24,7 +23,7 @@ router.post(
         const savedFile = await prisma.file.create({
             data: {
                 name: file.originalname,
-                path: file.originalname,
+                path: file.filename,
                 userId
             }
         })
@@ -33,6 +32,7 @@ router.post(
     }
 )
 
+//GET
 router.get("/", authMiddleware, async (req, res)=> {
     const userId = req.userId
 
@@ -60,7 +60,7 @@ router.get("/:id/download", authMiddleware, async (req, res)=> {
         return res.status(404).json({ error: "File not found"})
     }
 
-    const filePath = path.resolve("uploads", file.path)
+    const filePath = path.join(process.cwd(), "uploads", file.path)
 
     if (!fs.existsSync(filePath)) {
         return res.status(404).json( { error: "File not found in disk" } )
@@ -69,6 +69,27 @@ router.get("/:id/download", authMiddleware, async (req, res)=> {
     return res.download(filePath, file.name)
 })
 
+router.get("/public/:id", async (req, res)=> {
+    const { id } = req.params
+
+    const file = await prisma.file.findUnique({
+        where: { id }
+    })
+
+    if(!file) {
+        return res.status(404).json({ error: "File not found" })
+    }
+
+    const filePath = path.join(process.cwd(), "uploads", file.path)
+
+    if(!fs.existsSync(filePath)) {
+        res.status(404).json({ error: "File not found in disk" })
+    }
+
+    return res.download(filePath, file.name)
+})
+
+//DELETE
 router.delete("/:id", authMiddleware, async (req, res)=> {
     const { id } = (req as any).params
     const userId = req.userId
